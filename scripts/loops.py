6bb3a29793c065cd49408c5b5b5b85421e17eb7b
Bolci <Bolci@github.com>
2024-09-22 09:49:00 +0200

    script updated

--- scripts/loops.py
+++ scripts/loops.py
@@ -1,11 +1,12 @@
 import torch
+import numpy as np
 
 
 def train_loop(dataloader, model, loss_fn, optimizer, device="cuda"):
   size = len(dataloader.dataset)
   num_batches = len(dataloader)
   train_loss = 0.0
-  train_accuracy = 0.0
+    
   for batch, (X, y) in enumerate(dataloader):
     # Compute prediction and loss
     pred = model(X.to(device))
@@ -16,34 +17,34 @@ def train_loop(dataloader, model, loss_fn, optimizer, device="cuda"):
     optimizer.step()
 
     train_loss += loss.item()
-    train_accuracy += (pred.argmax(1) == y.to(device)).type(torch.float).sum().item()
 
-
-    if batch % 10000 == 0:
+    if batch % 1000 == 0:
       loss, current = loss.item(), batch * len(X)
       print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
       print()
 
   train_loss /= num_batches
-  train_accuracy /= size
-  return train_loss, train_accuracy
+  return train_loss
 
 
 def valid_loop(dataloader, model, loss_fn, metric_fn ,device="cuda"):
   size = len(dataloader.dataset)
   num_batches = len(dataloader)
-  test_loss, test_metric = 0, 0
+    
+  test_losses_to_print = []
+  test_metrics_to_print = []
 
   with torch.no_grad():
     for X, y in dataloader:
       pred = model(X.to(device))
-      test_loss += loss_fn(pred, y.to(device)).item()
-      test_metric += metric_fn(pred, y.to(device)).item()
+      test_loss = loss_fn(pred, y.to(device)).item()
+      test_metric = metric_fn(pred, y.to(device)).item()
       #test_accuracy += (pred.argmax(1) == y.to(device)).type(torch.float).sum().item()
+      test_losses_to_print.append(test_loss)
+      test_metrics_to_print.append(test_metric)
 
-  test_loss /= num_batches
-  test_metric /= size
-  print(f"valid Error: \n Accuracy: {test_metric}%, Avg loss: {test_loss:>8f} \n")
+  test_loss = np.mean(np.asarray(test_losses_to_print))
+  print(f"Avg loss: {test_loss:>8f} \n")
   return test_loss, test_metric
 
 
--- scripts/models/autoencoder_cnn.py
+++ scripts/models/autoencoder_cnn.py
@@ -3,29 +3,32 @@ import torch.nn as nn
 
 
 class CNNAutoencoder(nn.Module):
-    def __init__(self):
+    def __init__(self, no_channels = 1):
         super(CNNAutoencoder, self).__init__()
+        self.channels = no_channels
         
         # Encoder: 1D Convolution to reduce the sequence dimension
         self.encoder = nn.Sequential(
-            nn.Conv1d(2, 16, kernel_size=3, stride=2, padding=1),  # (2, 144) -> (16, 72)
+            nn.Conv1d(self.channels, 16, kernel_size=3, stride=1, padding=1),  
             nn.ReLU(True),
-            nn.Conv1d(16, 32, kernel_size=3, stride=2, padding=1),  # (16, 72) -> (32, 36)
+            nn.MaxPool1d(2),
+            nn.Conv1d(16, 32, kernel_size=3, stride=1, padding=1),  # (16, 72) -> (32, 36)
             nn.ReLU(True),
-            nn.Conv1d(32, 64, kernel_size=3, stride=2, padding=1),  # (32, 36) -> (64, 18)
+            nn.MaxPool1d(2),
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1),  # (32, 36) -> (64, 18)
             nn.ReLU(True),
-            nn.Conv1d(64, 128, kernel_size=3, stride=2, padding=1)  # (64, 18) -> (128, 9)
         )
         
         # Decoder: Upsample back to the original sequence
         self.decoder = nn.Sequential(
-            nn.ConvTranspose1d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),  # (128, 9) -> (64, 18)
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose1d(64, 32, kernel_size=3, stride=1, padding=1),   # (64, 18) -> (32, 36)
             nn.ReLU(True),
-            nn.ConvTranspose1d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),   # (64, 18) -> (32, 36)
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose1d(32, 16, kernel_size=3, stride=1, padding=1),   # (32, 36) -> (16, 72)
             nn.ReLU(True),
-            nn.ConvTranspose1d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),   # (32, 36) -> (16, 72)
-            nn.ReLU(True),
-            nn.ConvTranspose1d(16, 2, kernel_size=3, stride=2, padding=1, output_padding=1),    # (16, 72) -> (2, 144)
+            nn.ConvTranspose1d(16, self.channels, kernel_size=3, stride=1, padding=1),    # (16, 72) -> (2, 144)
+            nn.Sigmoid()
         )
     
     def forward(self, x):
