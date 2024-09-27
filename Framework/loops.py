d026622cf0fac2c85b5f1a67d63c6220c77051b1
Ondřej Zelený <zeleny.ondrej@email.cz>
2024-09-27 09:56:03 +0200

    Loops now contain labels, adding test loop

--- Framework/loops.py
+++ Framework/loops.py
@@ -1,12 +1,11 @@
 # This script contains important loops for the development pipeline
-
 import torch
 
 
 def train(model, train_loader, optimizer, criterion):
     model.train()
     total_loss = 0
-    for x in train_loader:
+    for x, y in train_loader:
         optimizer.zero_grad()
         output = model(x)
         loss = criterion(output, x)
@@ -20,8 +19,24 @@ def validate(model, val_loader, criterion):
     model.eval()
     total_loss = 0
     with torch.no_grad():
-        for x in val_loader:
+        for x, y in val_loader:
             output = model(x)
             loss = criterion(output, x)
             total_loss += loss.item()
     return total_loss / len(val_loader)
+
+
+def test(model, test_loader, criterion, threshold):
+    model.eval()
+    losses = []
+    labels = []
+    correct = 0
+    with torch.no_grad():
+        for x, y in test_loader:
+            output = model(x)
+            loss = criterion(output, x).item()
+            losses.append(loss)
+            labels.append(0 if loss < threshold else 1)
+            correct += 1 if (0 if loss < threshold else 1) == y else 0
+
+    return labels, losses, correct / len(test_loader)
