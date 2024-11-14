3e28d29c48ba70954c25dcaa2c727f8cb4befd7c
Bolci <Bolci@github.com>
2024-11-14 11:08:41 +0100

    data_path_worker corected

--- Framework/preprocessors/data_path_worker.py
+++ Framework/preprocessors/data_path_worker.py
@@ -6,25 +6,37 @@ def create_paths_for_data_type(path: str):
     return [os.path.join(path, x) for x in folders]
 
 
-def get_all_paths(path: dict) -> dict:
+def prepare_single_path_per_data_type(data_type, path):
     general_path = path['Data_path']
+    folder_paths_final = []
 
+    folders = path[data_type]
+    if not isinstance(folders, list):
+        folders = [folders]
+
+    for single_folder_path in folders:
+        single_folder_path_full = os.path.join(general_path, single_folder_path)
+        test_folders_path = create_paths_for_data_type(single_folder_path_full)
+
+        folder_paths_final += test_folders_path
+
+    return folder_paths_final
+
+
+def get_all_paths(path: dict) -> dict:
     train_folders_path = []
     valid_folders_path = []
-    test_folders_path = []
+    test_folders_paths = []
 
     if 'Train_folders' in path.keys():
-        train_path = os.path.join(general_path, path['Train_folders'])
-        train_folders_path = create_paths_for_data_type(train_path)
+        train_folders_path = prepare_single_path_per_data_type('Train_folders', path)
 
     if 'Valid_folders' in path.keys():
-        valid_path = os.path.join(general_path, path['Valid_folders'])
-        valid_folders_path = create_paths_for_data_type(valid_path)
+        valid_folders_path = prepare_single_path_per_data_type('Valid_folders', path)
 
     if 'Test_folders' in path.keys():
-        test_path = os.path.join(general_path, path['Test_folders'])
-        test_folders_path = create_paths_for_data_type(test_path)
+        test_folders_paths = prepare_single_path_per_data_type('Test_folders', path)
 
-    datasets = {'Train': train_folders_path, 'Valid': valid_folders_path, 'Test': test_folders_path}
+    datasets = {'Train': train_folders_path, 'Valid': valid_folders_path, 'Test': test_folders_paths}
 
     return datasets
